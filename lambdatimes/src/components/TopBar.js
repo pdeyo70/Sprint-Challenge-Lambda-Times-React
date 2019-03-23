67dfcd9f49e9bf19ad82de9b10d89e607909fe54
Paul DeYonghe <p.deyonghe@gmail.com>
2019-03-23 12:34:01 -0500

    got styled-components on TopBar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,79 @@
 import React from 'react';
+import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopBarInt>
+      <Container>
+        <ContLeft>
+          <CentSpan>TOPICS</CentSpan><CentSpan>SEARCH</CentSpan>
+        </ContLeft>
+        <ContCent>
+          <CentSpan>GENERAL</CentSpan>
+          <CentSpan>BROWNBAG</CentSpan>
+          <CentSpan>RANDOM</CentSpan>
+          <CentSpan>MUSIC</CentSpan>
+          <CentSpan>ANNOUNCEMENTS</CentSpan>
+        </ContCent>
+        <ContRight>
+          <CentSpan>LOG IN</CentSpan>
+        </ContRight>
+      </Container>
+    </TopBarInt>
   )
 }
+
+const TopBarInt = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`
+const ContLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`
+const ContCent = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: row;
+flex: 3;
+font-size: 9px;
+`
+const CentSpan = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+`
+const ContRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`
 
 export default TopBar;
