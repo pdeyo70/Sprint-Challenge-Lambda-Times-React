d49099fac5b212fea10e49aa1a6e94fc36ca97ec
Paul DeYonghe <p.deyonghe@gmail.com>
2019-03-23 12:11:37 -0500

    mostly done, save for work on styled-components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const Card = props => {
   return (
@@ -6,14 +7,17 @@ const Card = props => {
       <div className="headline">{/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={props.card.img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {/* author goes here */ props.card.author}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.object
+}
 
 export default Card;
